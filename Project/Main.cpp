3e0f39886ddcc380611b174e63b9a779d1f4ec0c
alexy <alexy@DESKTOP-HEA3SE1>
2021-03-01 17:27:10 +0100

    modif

--- Project/Main.cpp
+++ Project/Main.cpp
@@ -1,19 +1,37 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#pragma region variables
+sf::RenderWindow window;
+sf::CircleShape cercle;
+#pragma endregion varaibles
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
+int main(){
+    window.create(sf::VideoMode(800, 600), "Project Programmation");
+    window.setFramerateLimit(144);
+
+    std::cout << "Lancement du jeu" << std::endl;
+
+    cercle.setFillColor(sf::Color(100, 250, 150));
+    cercle.setRadius(100);
+    cercle.setPosition(300, 200);
+    cercle.setOutlineColor(sf::Color(100, 100, 100));
+    cercle.setOutlineThickness(2);
+
+
+    while (window.isOpen()){
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        window.clear();
+        window.draw(cercle);
+        window.display();
+        window.clear();
     }
 
     return 0;
