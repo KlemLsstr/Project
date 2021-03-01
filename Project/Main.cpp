e9fd410504cf4f37e02abbc0878edb52eae4aa56
alexy <alexy@DESKTOP-HEA3SE1>
2021-03-01 21:40:36 +0100

    modif

--- Project/Main.cpp
+++ Project/Main.cpp
@@ -6,17 +6,12 @@
 
 #pragma region variables
 
-sf::RenderWindow window;
-
-sf::RectangleShape Base;
-sf::RectangleShape rec1;
-sf::CircleShape milieu;
-
+sf::RenderWindow window; //Fenetre
+sf::RectangleShape Base; //Fond blanc
+sf::RectangleShape rec; //rectangle
+sf::CircleShape cercle;
 sf::Vector2i PosSouris;
-sf::Vector2f PosRec;
-
 int FX = 960, FY = 540;   //taille fenêtre en X et Y
-
 
 #pragma endregion varaibles
 
@@ -27,9 +22,6 @@ void gestion_souris();
 
 #pragma endregion entêtes
 
-
-
-
 int main(){
     window.create(sf::VideoMode(FX, FY), "Project Programmation");
     window.setFramerateLimit(144);
@@ -39,13 +31,13 @@ int main(){
     Base.setFillColor(sf::Color::White);
     Base.setSize(sf::Vector2f(FX, FY));
 
-    rec1 = rect((FX / 2)-25, ((FY / 2 ) - 12.5) + 100 , 50, 25);
-    rec1.setFillColor(sf::Color::Transparent);
-    rec1.setOutlineColor(sf::Color(0, 0, 0));
-    rec1.setOutlineThickness(2);
+    rec = rect((FX / 2)-25, ((FY / 2 ) - 12.5) + 100 , 50, 25);
+    rec.setFillColor(sf::Color::Transparent);
+    rec.setOutlineColor(sf::Color(100, 100, 100));
+    rec.setOutlineThickness(2);
 
-    milieu = cer(FX / 2 - 1, FY / 2 - 1, 1, 1000);
-    milieu.setFillColor(sf::Color::Black);
+    cercle = cer(FX / 2 - 1, FY / 2 - 1, 1, 1000);
+    cercle.setFillColor(sf::Color::Black);
 
     while (window.isOpen()){
         sf::Event event;
@@ -59,7 +51,7 @@ int main(){
         gestion_souris();
 
         window.draw(Base);
-        window.draw(rec1);
+        window.draw(rec);
         //window.draw(milieu);
         window.display();
         window.clear();
@@ -70,13 +62,13 @@ int main(){
 
 void gestion_clavier() {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-        rec1.move(0, -1);
+        rec.move(0, -1);
     }if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-        rec1.move(0, 1);
+        rec.move(0, 1);
     }if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-        rec1.move(1, 0);
+        rec.move(1, 0);
     }if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-        rec1.move(-1, 0);
+        rec.move(-1, 0);
     }
 }
 
@@ -84,13 +76,25 @@ void gestion_souris() {
     if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
 
         PosSouris = sf::Mouse::getPosition(window);
-        //rec1.setPosition(PosSouris.x - 100, PosSouris.y - 50);
 
-        PosRec = rec1.getPosition();
-        //std::cout << "Pos rectangle x: " << PosRec.x << " Pos rectangle y: " << PosRec.y << std::endl;
-        //std::cout << "Pos souris x: " << PosSouris.x << " Pos souris y: " << PosSouris.y << std::endl;
-        if (PosSouris.x >= PosRec.x && PosSouris.x <= PosRec.x + 50 && PosSouris.y >= PosRec.y && PosSouris.y <= PosRec.y + 25) {
-            std::cout << "Bienjouer" << std::endl;
+        //rec.setPosition(PosSouris.x - 25, PosSouris.y - 12.5);                                                                          //place le rectangle la ou on clique.
+        //std::cout << "Pos rectangle x: " << rec.getPosition().x << " Pos rectangle y: " << rec.getPosition().y << std::endl;            //Donne les positions du rectangle.
+        //std::cout << "Pos souris x: " << PosSouris.x << " Pos souris y: " << PosSouris.y << std::endl;                                  //Donne les positions de la souris
+
+        if (PosSouris.x >= rec.getPosition().x && PosSouris.x <= rec.getPosition().x + 50 && PosSouris.y >= rec.getPosition().y && PosSouris.y <= rec.getPosition().y + 25) {
+            std::cout << "Bienjouer" << std::endl;                                                                                        //Si l'utilisateur clique sur le rectangle (bouton) sa fait la suite....
+            do {
+                rec.setFillColor(sf::Color(202, 202, 202, 190));
+                rec.setOutlineColor(sf::Color::Black);
+                rec.setOutlineThickness(3);
+                window.clear();
+                window.draw(rec);
+            } while (sf::Mouse::isButtonPressed(sf::Mouse::Left));
+            rec.setFillColor(sf::Color::Transparent);
+            rec.setOutlineColor(sf::Color(100, 100, 100));
+            rec.setOutlineThickness(2);
+            window.clear();
+            window.draw(rec);
         }
     }
 }
